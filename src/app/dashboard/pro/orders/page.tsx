441e95b0a6e05c899ee5e71146c7c72d62437caf
towfiq-islam <tofiqislam03@gmail.com>
2026-01-02 15:05:24 +0600

    working in orders portion

--- src/app/dashboard/pro/orders/page.tsx
+++ src/app/dashboard/pro/orders/page.tsx
@@ -1,6 +1,51 @@
-import ProordersTabs from "@/Components/Common/DashboardReusable/PrordersTabs";
+"use client";
+import moment from "moment";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { getOrders } from "@/Hooks/api/dashboard_api";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { OrderRowSkeleton } from "@/Components/Loader/Loader";
+
+type orderItem = {
+  id: number;
+  order_number: string;
+  payment_method: string;
+  created_at: string;
+  total_quantity: number;
+  total_amount: string;
+  status: string;
+  shipping_option: string;
+  user: {
+    first_name: string;
+    last_name: string;
+    email: string;
+    id: number;
+  };
+};
 
 const page = () => {
+  const router = useRouter();
+  const [isActive, setIsActive] = useState("orders");
+  const [status, setStatus] = useState<string>("");
+  const [openPopup, setOpenPopup] = useState<boolean>(false);
+  const [orderId, setOrderId] = useState<number | null>(null);
+  console.log(orderId);
+  const tabs = ["orders", "pending", "confirmed", "delivered", "cancelled"];
+  const { data: myOrders, isLoading } = getOrders(status);
+  console.log(myOrders?.data);
+
+  useEffect(() => {
+    const handleWindowClick = () => {
+      setOpenPopup(false);
+    };
+
+    window.addEventListener("click", handleWindowClick);
+
+    return () => {
+      window.removeEventListener("click", handleWindowClick);
+    };
+  }, []);
+
   return (
     <>
       <div className="flex flex-wrap justify-between items-center gap-3.5 lg:gap-0">
@@ -41,8 +86,208 @@ const page = () => {
         </div> */}
       </div>
 
-      <div className="pt-9 pb-12">
-        <ProordersTabs />
+      {/* Tabs */}
+      <div className="flex flex-wrap justify-center sm:justify-start sm:flex-nowrap gap-2.5 sm:gap-5 md:gap-x-10">
+        {tabs?.map(tab => (
+          <h3
+            key={tab}
+            onClick={() => {
+              setIsActive(tab);
+              setStatus(tab === "orders" ? "" : tab);
+            }}
+            className={`cursor-pointer px-3 capitalize text-[16px] md:text-[20px] font-semibold ${
+              isActive === tab
+                ? "border-b-2 border-[#13141D] text-[#13141D]"
+                : "text-[#77978F]"
+            }`}
+          >
+            {tab}
+          </h3>
+        ))}
+      </div>
+
+      <div className="w-full pt-10">
+        {/* Desktop Table */}
+        <div className="hidden lg:block overflow-x-auto">
+          <table className="w-full border-collapse">
+            <thead>
+              <tr className="border-b-2 border-gray-300 text-[#13141D] text-[15px] xl:text-[16px] font-semibold">
+                <th className="py-3 px-4 text-left">Order #</th>
+                <th className="py-3 px-4 text-left">Order Date</th>
+                <th className="py-3 px-4 text-left">Customer</th>
+                <th className="py-3 px-4 text-left">Items</th>
+                <th className="py-3 px-4 text-left">Amount</th>
+                <th className="py-3 px-4 text-left">Status</th>
+                <th className="py-3 px-4 text-left">FullFillment</th>
+                <th className="py-3 px-4 text-center">Action</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {isLoading ? (
+                [1, 2, 3, 4, 5].map((_, idx) => <OrderRowSkeleton key={idx} />)
+              ) : myOrders?.data?.length > 0 ? (
+                myOrders?.data?.map((order: orderItem, i: number) => (
+                  <tr
+                    key={i}
+                    className="border-b border-gray-300 text-[#13141D] text-[14px] font-semibold"
+                  >
+                    <td className="py-4 px-4">{order?.order_number}</td>
+                    <td className="py-4 px-4">
+                      {moment(order?.created_at).format("ll")}
+                    </td>
+                    <td className="py-4 px-4">
+                      <div className="flex flex-col">
+                        <span>
+                          {order?.user?.first_name} {order?.user?.last_name}
+                        </span>
+                        <span className="text-sm text-gray-500">
+                          {order?.user?.email}
+                        </span>
+                      </div>
+                    </td>
+
+                    <td className="py-4 px-4">{order?.total_quantity}</td>
+                    <td className="py-4 px-4">${order?.total_amount}</td>
+                    <td className="py-4 px-4">
+                      <span
+                        className={`min-w-[100px] text-white capitalize inline-block text-center px-3 py-1 rounded-full text-sm font-semibold ${
+                          order?.status === "delivered"
+                            ? "bg-primary-green"
+                            : order?.status === "pending"
+                            ? "bg-accent-red"
+                            : order?.status === "pending"
+                            ? "bg-blue-500"
+                            : order?.status === "cancelled"
+                            ? "bg-red-500"
+                            : "bg-gray-500"
+                        }`}
+                      >
+                        {order?.status}
+                      </span>
+                    </td>
+                    <td className="py-4 px-4 capitalize">
+                      {order?.shipping_option}
+                    </td>
+
+                    <td className="py-4 px-4 flex justify-center items-center text-center relative border">
+                      <button
+                        onClick={e => {
+                          setOpenPopup(true);
+                          setOrderId(1);
+                        }}
+                        className="cursor-pointer border"
+                      >
+                        <BsThreeDotsVertical className="" />
+                      </button>
+
+                      {/* Popover */}
+                      <div
+                        onClick={e => e.stopPropagation()}
+                        className={`${
+                          openPopup && orderId === order?.id
+                            ? "opacity-100 scale-100"
+                            : "opacity-0 scale-90"
+                        } absolute duration-300 transition-all top-5 right-10 2xl:right-14 3xl:right-22 p-3 border border-gray-100 bg-white rounded-lg shadow-lg space-y-2.5 z-40 w-[150px] text-sm ${
+                          i === myOrders?.data?.length - 1 && "!-top-22"
+                        }`}
+                      >
+                        <button className="w-full text-left px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                          View Details
+                        </button>
+                        <button className="w-full text-left px-4 py-2 hover:bg-gray-100 text-red-500 cursor-pointer">
+                          Canceled
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <p className="text-red-500 font-semibold text-lg mt-5">
+                  No orders found
+                </p>
+              )}
+            </tbody>
+          </table>
+        </div>
+
+        {/* Mobile Card */}
+        {/* <div className="lg:hidden space-y-4">
+                 {paginatedData.map((order, i) => (
+                   <div key={i} className=" rounded-lg  overflow-hidden relative">
+                     <div className="flex justify-between items-center  px-4 py-2">
+                       <div>
+                         <p className="font-semibold">#{order.id}</p>
+                         <p className="text-xs text-gray-500">{order.date}</p>
+                       </div>
+     
+                       <div className="flex items-center gap-2">
+                         <span
+                           className={`px-2 py-1 rounded-full text-xs font-medium ${
+                             statusColors[order.status] ?? "bg-gray-300 text-black"
+                           }`}
+                         >
+                           {order.status}
+                         </span>
+     
+                         <div className="relative">
+                           <BsThreeDotsVertical
+                             onClick={() => toggleDropdown(i)}
+                             className="cursor-pointer"
+                           />
+                           {openRow === i && (
+                             <div className="absolute right-0 mt-2 w-28 bg-white rounded shadow-lg z-10">
+                               <button
+                                 className="w-full text-left px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                                 onClick={() => setOpenRow(null)}
+                               >
+                                 Edit
+                               </button>
+                               <button
+                                 className="w-full text-left px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                                 onClick={() => {
+                                   router.push(`/dashboard/pro/orders/${order.id}`);
+                                 }}
+                               >
+                                 View Details
+                               </button>
+                               <button
+                                 className="w-full text-left px-4 py-2 hover:bg-gray-100 text-red-500 cursor-pointer"
+                                 onClick={() => setOpenRow(null)}
+                               >
+                                 Canceled
+                               </button>
+                             </div>
+                           )}
+                         </div>
+                       </div>
+                     </div>
+     
+                     <div className="text-sm">
+                       <div className="flex justify-between px-4 py-2 bg-[#F0EEE9]">
+                         <span className="text-gray-600">Customer</span>
+                         <span className="font-medium">{order.customer}</span>
+                       </div>
+                       <div className="flex justify-between px-4 py-2">
+                         <span className="text-gray-600">Email</span>
+                         <span className="text-gray-800">{order.email}</span>
+                       </div>
+                       <div className="flex justify-between px-4 py-2 bg-[#F0EEE9]">
+                         <span className="text-gray-600">Opt In</span>
+                         <span className="font-medium">{order.optIn}</span>
+                       </div>
+                       <div className="flex justify-between px-4 py-2">
+                         <span className="text-gray-600">Items</span>
+                         <span className="font-medium">{order.items}</span>
+                       </div>
+                       <div className="flex justify-between px-4 py-2 bg-[#F0EEE9]">
+                         <span className="text-gray-600">Amount</span>
+                         <span className="font-medium">{order.amount}</span>
+                       </div>
+                     </div>
+                   </div>
+                 ))}
+               </div> */}
       </div>
     </>
   );

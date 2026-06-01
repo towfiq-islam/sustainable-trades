ed2dee070a50efb65c1e611761df59b273e162fb
towfiq-islam <tofiqislam03@gmail.com>
2026-06-01 08:26:25 +0600

    modified cms.api

--- src/Hooks/api/cms_api.ts
+++ src/Hooks/api/cms_api.ts
@@ -95,7 +95,7 @@ export async function getAllShops() {
 export async function getMissionData() {
   return useServerApi({
     endpoint: "/api/our-mission",
-    revalidate: 60,
+    revalidate: 3600,
   });
 }
 

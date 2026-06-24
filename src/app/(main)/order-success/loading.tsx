9de7b5d61383b83e7afdec85299ca0e0934bc2ab
towfiq-islam <tofiqislam03@gmail.com>
2026-06-24 14:44:23 +0600

    removed loading.tsx file

--- src/app/(main)/order-success/loading.tsx
+++ src/app/(main)/order-success/loading.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { PuffLoader } from "react-spinners";
 
 const loading = () => {
---
+++ src/app/auth/create-shop/loading.tsx
@@ -0,0 +1,11 @@
+import { PuffLoader } from "react-spinners";
+
+const loading = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <PuffLoader color="#274f45" />
+    </div>
+  );
+};
+
+export default loading;

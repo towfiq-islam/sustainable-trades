c7c4b45339ad09fd6451508312793339cf625e06
towfiq-islam <tofiqislam03@gmail.com>
2026-07-21 12:49:57 +0600

    feat: working in proxy

--- src/lib/cms.api.ts
+++ src/lib/cms.api.ts
@@ -1,129 +1,142 @@
-import { fetchServerData } from "@/lib/fetchServerData";
+import { serverFetch } from "@/lib/serverFetch";
 
 // Membership Spotlight
 export async function getSpotlightData() {
-  return fetchServerData({
+  return serverFetch({
     endpoint: "/api/spotlight-applications",
-    ssr: true,
+    mode: "SSR",
   });
 }
 
 // Site Settings
 export async function getSiteSettings() {
-  return fetchServerData({
+  return serverFetch({
     endpoint: "/api/site-settings",
+    mode: "ISR",
     revalidate: 86400,
   });
 }
 
 // Get Social Links
 export async function getSocialLinks() {
-  return fetchServerData({
+  return serverFetch({
     endpoint: "/api/social-links",
+    mode: "ISR",
     revalidate: 86400,
   });
 }
 
 // Get Banner
 export async function getBannerData() {
-  return fetchServerData({
+  return serverFetch({
     endpoint: "/api/banners",
+    mode: "ISR",
     revalidate: 3600,
   });
 }
 
 // Get Contact
 export async function getContactData() {
-  return fetchServerData({
+  return serverFetch({
     endpoint: "/api/contact",
+    mode: "ISR",
     revalidate: 3600,
   });
 }
 
 // Get Terms
 export async function getTermsData() {
-  return fetchServerData({
+  return serverFetch({
     endpoint: "/api/terms-and-conditions",
+    mode: "ISR",
     revalidate: 3600,
   });
 }
 
 // Get Infringement
 export async function getInfringementData() {
-  return fetchServerData({
+  return serverFetch({
     endpoint: "/api/infringement-report",
+    mode: "ISR",
     revalidate: 3600,
   });
 }
 
 // How It Works
 export async function getHowItWorksData() {
-  return fetchServerData({
+  return serverFetch({
     endpoint: "/api/how-it-works",
+    mode: "ISR",
     revalidate: 3600,
   });
 }
 
 // Product Categories
 export async function getProductCategories() {
-  return fetchServerData({
+  return serverFetch({
     endpoint: "/api/categories",
-    ssr: true,
+    mode: "SSR",
   });
 }
 
 // All Shops
 export async function getAllShops() {
-  return fetchServerData({
+  return serverFetch({
     endpoint: "/api/shops",
-    ssr: true,
+    mode: "SSR",
   });
 }
 
 // Get Mission Data
 export async function getMissionData() {
-  return fetchServerData({
+  return serverFetch({
     endpoint: "/api/our-mission",
+    mode: "ISR",
     revalidate: 3600,
   });
 }
 
 // Dynamic Pages
 export async function getDynamicPages() {
-  return fetchServerData({
+  return serverFetch({
     endpoint: "/api/dynamic-pages",
+    mode: "ISR",
     revalidate: 3600,
   });
 }
 
 // Single Dynamic page
 export async function getSingleDynamicPage(slug: string) {
-  return fetchServerData({
+  return serverFetch({
     endpoint: `/api/dynamic-pages/single/${slug}`,
+    mode: "ISR",
     revalidate: 3600,
   });
 }
 
 // Blogs
 export async function getBlogs() {
-  return fetchServerData({
+  return serverFetch({
     endpoint: "/api/blogs",
     revalidate: 3600,
+    mode: "ISR",
   });
 }
 
 // Single Blog
 export async function getSingleBlog(id: number) {
-  return fetchServerData({
+  return serverFetch({
     endpoint: `/api/blog/${id}`,
+    mode: "ISR",
     revalidate: 3600,
   });
 }
 
 // Get FAQ
 export const getFAQ = () => {
-  return fetchServerData({
+  return serverFetch({
     endpoint: "/api/faq/all",
+    mode: "ISR",
     revalidate: 3600,
   });
 };

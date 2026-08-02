d0f59e1d0363be2018e638423c63820f73bf78f5
towfiq-islam <tofiqislam03@gmail.com>
2026-08-02 11:24:42 +0600

    resolved shipping address form

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,17 +1,12 @@
 import DashboardLayoutClient from "@/Shared/DashboardLayoutClient";
 import { getUser } from "@/lib/getUser";
-import { serverFetch } from "@/lib/serverFetch";
 
 export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const initialUser = await serverFetch({
-    endpoint: "/api/users/data",
-    mode: "SSR",
-  });
-  console.log("initialUser", initialUser);
+  const initialUser = await getUser();
 
   return (
     <DashboardLayoutClient initialUser={initialUser?.data}>
